7b3983695ffa95d5bf779ad81c233f71d6e04e2f
Jose Morel <jose@Joses-MBP.lan>
2024-11-19 11:50:59 -0500

    eslint update

--- lua/lsp/eslint.lua
+++ lua/lsp/eslint.lua
@@ -1,16 +1,3 @@
-local customizations = {
-  { rule = 'style/*',   severity = 'off', fixable = true },
-  { rule = 'format/*',  severity = 'off', fixable = true },
-  { rule = '*-indent',  severity = 'off', fixable = true },
-  { rule = '*-spacing', severity = 'off', fixable = true },
-  { rule = '*-spaces',  severity = 'off', fixable = true },
-  { rule = '*-order',   severity = 'off', fixable = true },
-  { rule = '*-dangle',  severity = 'off', fixable = true },
-  { rule = '*-newline', severity = 'off', fixable = true },
-  { rule = '*quotes',   severity = 'off', fixable = true },
-  { rule = '*semi',     severity = 'off', fixable = true },
-}
-
 return {
   filetypes = {
     "javascript",
@@ -38,7 +25,22 @@ return {
     "postcss"
   },
   settings = {
-    -- Silent the stylistic rules in you IDE, but still auto fix them
-    rulesCustomizations = customizations,
+    eslint = {
+      -- Silent the stylistic rules in you IDE, but still auto fix them
+      rulesCustomizations = {
+        { rule = 'style/*',   severity = 'off', fixable = true },
+        { rule = 'format/*',  severity = 'off', fixable = true },
+        { rule = '*-indent',  severity = 'off', fixable = true },
+        { rule = '*-spacing', severity = 'off', fixable = true },
+        { rule = '*-spaces',  severity = 'off', fixable = true },
+        { rule = '*-order',   severity = 'off', fixable = true },
+        { rule = '*-dangle',  severity = 'off', fixable = true },
+        { rule = '*-newline', severity = 'off', fixable = true },
+        { rule = '*quotes',   severity = 'off', fixable = true },
+        { rule = '*semi',     severity = 'off', fixable = true },
+      },
+      -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
+      workingDirectories = { mode = "auto" },
+    }
   },
 }
